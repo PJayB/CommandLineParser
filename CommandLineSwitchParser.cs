5d4b2ecde637c6d8e5d1f90013571f9d670eb8c5
Pete Lewis <pete.lewis@unity3d.com>
2017-08-23 09:33:14 -0700

    Rename namespace

--- CommandLineSwitchParser.cs
+++ CommandLineSwitchParser.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Reflection;
 using System.Collections;
 
-namespace FissionBuildAndDeploy
+namespace Utilities
 {
     class CommandLineSwitchParser
     {
